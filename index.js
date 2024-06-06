d93c29ca10fa9934857a1b2d45c4519e8a892eb5
AnasCoding1986 <anassust.scw@gmail.com>
2024-06-06 18:29:39 +0600

    notification post done

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
         res.send(result);
     })
 
+    // notification
+    app.post('/notification', async(req,res) => {
+      const singleNotification = req.body;
+      const result = await notificationCollection.insertOne(singleNotification);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
