da1f5e0a364995551744c11e7d555d09c06d46f3
AnasCoding1986 <anassust.scw@gmail.com>
2024-06-12 02:32:17 +0300

    cookingwith jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdajqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,28 +27,89 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("DialogueDock").collection("users");
     const allMsgCollection = client.db("DialogueDock").collection("allMsg");
     const notificationCollection = client.db("DialogueDock").collection("notification");
 
-    // allMsg
-    app.get('/allMsg', async(req,res) => {
-        const result = await allMsgCollection.find().toArray();
-        res.send(result);
+    // jwt related
+    app.post('/jwt', async(req,res)=> {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({token})
     })
 
-    app.post('/allMsg', async(req,res) => {
+    // middlewire
+    const verifyToken = (req,res,next) => {
+      console.log('inside verify token',req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded) => {
+        if (err) {
+          return res.status(401).send({message: 'forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+      })
+      // next();
+    }
+
+
+    // users
+    app.get('/users',verifyToken, async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      // insert email if users does not exists
+      const query = { email : user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({message: 'user already exists', insertedId: null})
+      };
+
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await userCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+
+    // allMsg
+    app.get('/allMsg', async (req, res) => {
+      const result = await allMsgCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/allMsg', async (req, res) => {
       const msg = req.body;
       const result = await allMsgCollection.insertOne(msg);
       res.send(result);
     })
 
     // notification
-    app.get('/notification', async(req,res) => {
+    app.get('/notification', async (req, res) => {
       const result = await notificationCollection.find().toArray();
       res.send(result);
     })
 
-    app.post('/notification', async(req,res) => {
+    app.post('/notification', async (req, res) => {
       const singleNotification = req.body;
       const result = await notificationCollection.insertOne(singleNotification);
       res.send(result);
@@ -64,10 +126,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res) => {
-    res.send('DialogueDock is running')
+app.get('/', (req, res) => {
+  res.send('DialogueDock is running')
 })
 
 app.listen(port, () => {
-    console.log(`DialogueDock is sitting on port ${port}`);
+  console.log(`DialogueDock is sitting on port ${port}`);
 })
