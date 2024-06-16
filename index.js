a31991cbff2f741ffa0b442edfe18dd4d726a179
AnasCoding1986 <anassust.scw@gmail.com>
2024-06-16 17:08:51 +0600

    working with msg details

--- index.js
+++ index.js
@@ -137,6 +137,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/allMsg/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allMsgCollection.findOne(query);
+      res.send(result)
+    })
+
     app.post('/allMsg', async (req, res) => {
       const msg = req.body;
       const result = await allMsgCollection.insertOne(msg);
