0f2c7eba43caee3384122625923bfa27614bdeb7
AnasCoding1986 <anassust.scw@gmail.com>
2024-06-22 18:40:25 +0600

    some final correction

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("DialogueDock").collection("users");
     const allMsgCollection = client.db("DialogueDock").collection("allMsg");
@@ -197,8 +197,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
